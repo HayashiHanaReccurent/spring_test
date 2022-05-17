71194f35bbeca4f86ea49f65daf3d41a89042705
Hana Hayashi <hanahayashi0706@gmail.com>
2022-05-17 15:06:22 +0900

    ADD HAYASHI FizzBuzzControllerを追加

---
+++ src/main/java/com/example/demo/FizzBuzzController.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FizzBuzzController {
+
+	// <form action="/apply" method="post">
+	@RequestMapping(value = "/apply", method = RequestMethod.POST)
+	public ModelAndView apply(@RequestParam("num") int num, ModelAndView mv) {
+
+		// Fizz Buzzの処理
+		// 結果を格納する変数を作る
+		String result = "";
+
+		// 3の倍数の場合
+		if (num % 3 == 0 && !(num == 0)) {
+			result += "Fizz";
+		}
+
+		// 5の倍数の場合
+		if (num % 5 == 0 && !(num == 0)) {
+			result += "Buzz";
+		}
+
+		// 7の倍数の場合
+		if (num % 7 == 0 && !(num == 0)) {
+			result += "Ass";
+		}
+
+		// 上記のどれにも当てはまらない場合
+		if (result.length() == 0) {
+			result += "⊂ﾟU┬──────────────┬";
+		}
+
+		// 結果を設定
+		mv.addObject("result", result);
+		mv.addObject("num", num);
+
+		// 表示するHTMLファイルの設定
+		mv.setViewName("index");
+		return mv;
+	}
+}
